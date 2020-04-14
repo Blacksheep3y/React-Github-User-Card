7d322df07b2fdedd6a1076d6aafa3b6f798a1e1e
Matthew Castillo <blacksheep3y@gmail.com>
2020-04-14 14:11:15 -0400

    Was finally able to get something to render thanks to the help of some pair programming

--- another-github-app/src/App.js
+++ another-github-app/src/App.js
@@ -13,8 +13,10 @@ class App extends React.Component {
   componentDidMount() {
     console.log('componentDidMount');
     fetch('https://api.github.com/users/Blacksheep3y')
-    .then(res => res.data.json())
-    .then(githubber => this.setState({ github: githubber.avatar_url }))
+    // .then(res => console.log('Before: ', res))
+    .then(res => res.json())
+    // .then(res => console.log('After: ', res))
+    .then(githubber => {this.setState({ github: githubber})})
     .catch(err => {
       console.log("Err: ", err);
     });
@@ -32,15 +34,18 @@ class App extends React.Component {
   };
   
   render() {
-    console.log(this.state);
+    // console.log(this.state);
     return (
       <div>
         <h1>Hello Github User!</h1>
-        <div className="github">
+        {/* <div className="github">
           {this.state.github.map(githubber => (
             <img width="200" src={githubber} alt={githubber} />
           ))}
-        </div>
+        </div> */}
+        <p>{this.state.github.login}</p>
+        <img src={this.state.github.avatar_url} />
+        {console.log('THIS IS this.state:', this.state) }
       </div>
     );
   }
